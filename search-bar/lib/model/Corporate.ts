972cff46acb5d74688649d9b3ff6a6d561697d8f
resetmerlin <resetmerlin@gmail.com>
2023-12-05 12:23:09 +0900

    feat: Add Corporate model

---
+++ search-bar/lib/model/Corporate.ts
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const corporateSchema = new Schema({
+  회사명: {
+    type: String,
+    required: true,
+  },
+  종목코드: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  업종: {
+    type: String,
+    required: true,
+  },
+  주요제품: {
+    type: String,
+    required: true,
+  },
+  상장일: {
+    type: String,
+    required: true,
+  },
+  결산월: {
+    type: String,
+    required: true,
+  },
+  대표자명: {
+    type: String,
+    required: true,
+  },
+  홈페이지: {
+    type: String,
+    required: false,
+  },
+  지역: {
+    type: String,
+    required: true,
+  },
+});
+
+const Corporate =
+  mongoose.models.corporate || mongoose.model('corporate', corporateSchema);
+
+export default Corporate;
---
+++ search-bar/lib/model/index.ts
@@ -0,0 +1,1 @@
+export { default as Corporate } from './Corporate';
