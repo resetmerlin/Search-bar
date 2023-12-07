290b9053f333246681cb7fdf9859ee5fecd69813
resetmerlin <resetmerlin@gmail.com>
2023-12-07 13:21:36 +0900

    feat: Add getValueOfDropDown func props

--- search-bar/components/searchDropdown/content/SearchDropDown.tsx
+++ search-bar/components/searchDropdown/content/SearchDropDown.tsx
@@ -1,10 +1,18 @@
 import { ICorporate } from '@/lib/types/dto';
 import React from 'react';
 
-export default function SearchDropDown({ 회사명 }: Pick<ICorporate, '회사명'>) {
+type IProps = {
+  회사명: string;
+  getValueOfDropDown: (e: React.MouseEvent<HTMLButtonElement>) => void;
+};
+
+export default function SearchDropDown({ 회사명, getValueOfDropDown }: IProps) {
   return (
-    <div className="block w-full p-4 ps-10 text-sm text-gray-900   bg-gray-50  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:bg-gray-100	 ">
+    <button
+      onClick={getValueOfDropDown}
+      className="block w-full p-4 ps-10 text-sm text-gray-900  text-left	 bg-gray-50  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white hover:bg-gray-100	 "
+    >
       {회사명}
-    </div>
+    </button>
   );
 }
