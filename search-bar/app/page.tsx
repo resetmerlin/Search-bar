7f151fdef9b45218c61a7a0421cdd8ff444f543b
resetmerlin <resetmerlin@gmail.com>
2023-12-07 13:16:01 +0900

    feat: Add fetching coporates, getting value of DropDown func

--- search-bar/app/page.tsx
+++ search-bar/app/page.tsx
@@ -1,54 +1,77 @@
 'use client';
 
+import { useEffect, useMemo, useRef, useState } from 'react';
+import useSWRMutation from 'swr/mutation';
+import axios from 'axios';
+
 import {
   SearchBar,
   SearchDropDown,
   SearchDropDownContainer
 } from '@/components';
-import { useEffect, useState } from 'react';
+import { ICorporate } from '@/lib';
 import { useDebounce } from './hooks';
-import { data } from './dummyData';
-import { ICorporate, ICorporates } from '@/lib/types/dto';
+
+async function updateCorporates(url: string, { arg }: { arg: string }) {
+  return axios.get(url + arg).then((res) => res.data);
+}
 
 export default function Home() {
   const [search, setSearch] = useState('');
-  const [corporates, setCorporates] = useState<ICorporates | null>(null);
+  const searchRef = useRef<HTMLInputElement>(null);
+  const { trigger, data } = useSWRMutation(
+    '/api/search?title=',
+    updateCorporates
+  );
 
+  /** Extract 5 datas for Search Dropdown*/
+  const fiveCorporates = useMemo(() => {
+    return data && [...data].slice(0, 5);
+  }, [data]);
+
+  /** Use debounce for setting a search terms*/
   const handleSearch = useDebounce((term) => {
     setSearch(term);
   }, 700);
 
+  /** get Value of Drop down test via click */
+  const getValueOfDropDown = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (!e.currentTarget.textContent) return null;
+
+    setSearch(e.currentTarget.textContent);
+
+    if (searchRef.current)
+      searchRef.current.value = e.currentTarget.textContent;
+  };
+
+  /** search input handler*/
   const searchHandler = (e: React.FormEvent<HTMLInputElement>) => {
     const term = e.currentTarget.value;
     handleSearch(term);
   };
 
+  /** Hook for passively fetching corporates based on search input */
   useEffect(() => {
-    if (search.length > 0) {
-      const filteredData =
-        data &&
-        data
-          .filter((corporate) => {
-            const regex = new RegExp(search, 'i');
-            return regex.test(corporate.회사명);
-          })
-          .slice(0, 5);
-
-      setCorporates(filteredData);
-    } else {
-      setCorporates(null);
+    let ignore = false;
+    if (search.length > 0 && !ignore) {
+      trigger(search);
     }
-  }, [search]);
+    return () => {
+      ignore = true;
+    };
+  }, [trigger, search]);
 
   return (
     <>
-      <SearchBar searchHandler={searchHandler}>
+      <SearchBar searchHandler={searchHandler} ref={searchRef}>
         <SearchDropDownContainer
           className={!search ? 'opacity-0' : 'opacity-100'}
         >
-          {corporates &&
-            corporates.map((corporate: ICorporate) => (
+          {fiveCorporates &&
+            fiveCorporates.map((corporate: ICorporate) => (
               <SearchDropDown
+                getValueOfDropDown={getValueOfDropDown}
                 회사명={corporate.회사명}
                 key={corporate.종목코드}
               />
