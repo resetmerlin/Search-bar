a72a50c2f92446e1b657b8a2d5a27251df093012
resetmerlin <resetmerlin@gmail.com>
2023-12-08 10:23:24 +0900

    feat: Add optional chaining to prevent error

--- search-bar/lib/model/Corporate.ts
+++ search-bar/lib/model/Corporate.ts
@@ -5,44 +5,44 @@ const { Schema } = mongoose;
 const corporateSchema = new Schema({
   회사명: {
     type: String,
-    required: true,
+    required: true
   },
   종목코드: {
     type: Number,
     required: true,
-    unique: true,
+    unique: true
   },
   업종: {
     type: String,
-    required: true,
+    required: true
   },
   주요제품: {
     type: String,
-    required: true,
+    required: true
   },
   상장일: {
     type: String,
-    required: true,
+    required: true
   },
   결산월: {
     type: String,
-    required: true,
+    required: true
   },
   대표자명: {
     type: String,
-    required: true,
+    required: true
   },
   홈페이지: {
     type: String,
-    required: false,
+    required: false
   },
   지역: {
     type: String,
-    required: true,
-  },
+    required: true
+  }
 });
 const Corporate =
-  mongoose.models.Corporate ||
+  mongoose.models?.Corporate ||
   mongoose.model('Corporate', corporateSchema, 'search');
 
 export default Corporate;
