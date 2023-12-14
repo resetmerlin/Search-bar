64ab336d4b973ad86e380abe4fd4ab11a468765e
resetmerlin <resetmerlin@gmail.com>
2023-12-14 11:44:19 +0900

    refactor: Clean Search props and delete hooks

--- search-bar/app/page.tsx
+++ search-bar/app/page.tsx
@@ -1,26 +1,5 @@
-'use client';
-
 import { Search } from '@/components';
-import { useSearchHook } from './hooks';
 
 export default function Home() {
-  const [
-    searchHandler,
-    searchRef,
-    search,
-    getValueOfDropDown,
-    fiveCorporates,
-    goCorporatepage
-  ] = useSearchHook();
-
-  return (
-    <Search
-      searchHandler={searchHandler}
-      ref={searchRef}
-      search={search}
-      getValueOfDropDown={getValueOfDropDown}
-      fiveCorporates={fiveCorporates}
-      goCorporatepage={goCorporatepage}
-    />
-  );
+  return <Search />;
 }
